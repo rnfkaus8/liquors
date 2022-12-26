f11b82faca8083d74f5ab3acfb6ce334ce567709
rnfkaus8 <kth4350@gmail.com>
2022-12-26 22:28:34 +0900

    주류 응답 객체 추가

---
+++ src/main/kotlin/record/liquors/liquor/api/LiquorResponse.kt
@@ -0,0 +1,28 @@
+package record.liquors.liquor.api
+
+import record.liquors.liquor.entity.Liquor
+import record.liquors.liquor.entity.LiquorRating
+
+data class LiquorResponse(
+    val name: String,
+
+    val rating: LiquorRating,
+
+    var review: String?,
+
+    var category: String?,
+
+    var id: Long? = null
+) {
+    companion object {
+        fun toDto(liquor: Liquor): LiquorResponse {
+            return LiquorResponse(
+                name = liquor.name,
+                rating = liquor.rating,
+                review = liquor.review,
+                category = liquor.category,
+                id = liquor.id
+            )
+        }
+    }
+}
