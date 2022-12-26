744a0fbec41169eed09cbfc4048b5eef8aee19bb
rnfkaus8 <kth4350@gmail.com>
2022-12-26 22:28:55 +0900

    모든 주류 데이터 조회 메소드 추가

---
+++ src/main/kotlin/record/liquors/liquor/api/LiquorApiController.kt
@@ -0,0 +1,16 @@
+package record.liquors.liquor.api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import record.liquors.liquor.service.LiquorService
+
+@RestController
+class LiquorApiController(
+    val liquorService: LiquorService
+) {
+    @GetMapping("/liquors")
+    fun getLiquorList(): List<LiquorResponse> {
+        return liquorService.findLiquors()
+    }
+
+}
--- src/main/kotlin/record/liquors/liquor/service/LiquorService.kt
+++ src/main/kotlin/record/liquors/liquor/service/LiquorService.kt
@@ -3,6 +3,7 @@ package record.liquors.liquor.service
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import record.liquors.liquor.api.LiquorResponse
 import record.liquors.liquor.entity.Liquor
 import record.liquors.liquor.repository.LiquorRepository
 import java.util.NoSuchElementException
@@ -24,5 +25,9 @@ class LiquorService(
       throw NoSuchElementException("liquor not found")
     }
     return liquor.get()
+  }
+
+  fun findLiquors(): List<LiquorResponse> {
+    return liquorRepository.findAll().stream().map { liquor -> LiquorResponse.toDto(liquor) }.toList()
   }
 }
