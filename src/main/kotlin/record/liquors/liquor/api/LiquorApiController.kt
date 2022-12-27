b64a6722cc0acc62d53eec7b9eba74411a73cb19
rnfkaus8 <kth4350@gmail.com>
2022-12-27 21:00:20 +0900

    주류 저장 controller 추가

--- src/main/kotlin/record/liquors/liquor/api/LiquorApiController.kt
+++ src/main/kotlin/record/liquors/liquor/api/LiquorApiController.kt
@@ -1,6 +1,7 @@
 package record.liquors.liquor.api
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import record.liquors.liquor.service.LiquorService
 
@@ -13,4 +14,8 @@ class LiquorApiController(
         return liquorService.findLiquors()
     }
 
+    @PostMapping("/liquor")
+    fun save(request: LiquorSaveRequest) {
+        liquorService.save(request)
+    }
 }
