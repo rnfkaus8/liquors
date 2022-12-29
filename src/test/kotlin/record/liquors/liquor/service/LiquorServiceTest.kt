b9799b78a270018325f25baf73658c4577ad978e
rnfkaus8 <kth4350@gmail.com>
2022-12-29 22:00:11 +0900

    request 객체 추가에 따른 수정

--- src/test/kotlin/record/liquors/liquor/service/LiquorServiceTest.kt
+++ src/test/kotlin/record/liquors/liquor/service/LiquorServiceTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import record.liquors.liquor.api.LiquorSaveRequest
 import record.liquors.liquor.entity.Liquor
 import record.liquors.liquor.entity.LiquorRating
 
@@ -18,7 +19,7 @@ class LiquorServiceTest(
     @Test
     fun save() {
         val liquor = Liquor(name = "버팔로 트레이스", LiquorRating.VERY_GOOD, review = "review", category = "cate")
-        liquorService.save(liquor)
+        liquorService.save(LiquorSaveRequest(name = liquor.name, rating = liquor.rating, review = liquor.review, category = liquor.category))
 
         val findLiquor = liquorService.findById(liquor.id!!)
 
