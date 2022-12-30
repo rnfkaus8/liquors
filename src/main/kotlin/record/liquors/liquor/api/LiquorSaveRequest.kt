e0276b7fe6e0d1bb346a16e027d6fc89426f0687
rnfkaus8 <kth4350@gmail.com>
2022-12-30 21:55:54 +0900

    validation 추가

--- src/main/kotlin/record/liquors/liquor/api/LiquorSaveRequest.kt
+++ src/main/kotlin/record/liquors/liquor/api/LiquorSaveRequest.kt
@@ -2,9 +2,12 @@ package record.liquors.liquor.api
 
 import record.liquors.liquor.entity.Liquor
 import record.liquors.liquor.entity.LiquorRating
+import javax.validation.constraints.NotBlank
 
 data class LiquorSaveRequest(
+    @NotBlank
     val name: String,
+    @NotBlank
     val rating: LiquorRating,
     var review: String?,
     var category: String?,
