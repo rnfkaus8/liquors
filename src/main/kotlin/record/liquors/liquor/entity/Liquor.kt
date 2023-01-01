0112444307d4b0526a80d5bcee74382769ebf0fc
rnfkaus8 <kth4350@gmail.com>
2023-01-01 20:34:08 +0900

    수정 메소드 추가

--- src/main/kotlin/record/liquors/liquor/entity/Liquor.kt
+++ src/main/kotlin/record/liquors/liquor/entity/Liquor.kt
@@ -1,15 +1,16 @@
 package record.liquors.liquor.entity
 
 import record.liquors.base.BaseEntityModel
+import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
 class Liquor(
 
-    val name: String,
+    var name: String,
 
     @Enumerated(EnumType.STRING)
-    val rating: LiquorRating,
+    var rating: LiquorRating,
 
     var review: String?,
 
@@ -17,4 +18,12 @@ class Liquor(
 
     @Id @GeneratedValue
     var id: Long? = null
-) : BaseEntityModel()
+) : BaseEntityModel() {
+    fun update(name: String, rating: LiquorRating, review: String?, category: String?) {
+        this.name = name
+        this.rating = rating
+        this.review = review
+        this.category = category
+        this.updatedAt = LocalDateTime.now()
+    }
+}
