c26d741a1e299227726facee3fc4ade7d1cced02
rnfkaus8 <kth4350@gmail.com>
2023-01-03 20:15:23 +0900

    add test : findLiquors service

--- src/test/kotlin/record/liquors/liquor/service/LiquorServiceTest.kt
+++ src/test/kotlin/record/liquors/liquor/service/LiquorServiceTest.kt
@@ -1,19 +1,22 @@
 package record.liquors.liquor.service
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import record.liquors.liquor.api.LiquorResponse
 import record.liquors.liquor.api.LiquorSaveRequest
 import record.liquors.liquor.entity.Liquor
 import record.liquors.liquor.entity.LiquorRating
+import record.liquors.liquor.repository.LiquorRepository
 
 @SpringBootTest
 class LiquorServiceTest(
-    @Autowired val liquorService: LiquorService
+    @Autowired val liquorService: LiquorService,
+    @Autowired val liquorRepository: LiquorRepository
 ) {
 
     @Test
@@ -21,8 +24,24 @@ class LiquorServiceTest(
         val liquor = Liquor(name = "버팔로 트레이스", LiquorRating.VERY_GOOD, review = "review", category = "cate")
         liquorService.save(LiquorSaveRequest(name = liquor.name, rating = liquor.rating, review = liquor.review, category = liquor.category))
 
-        val findLiquor = liquorService.findById(liquor.id!!)
+        val findLiquor = liquorService.findOne(liquor.id!!)
 
         assertThat(findLiquor.id).isEqualTo(liquor.id)
     }
+
+    @Test
+    fun findAll() {
+        val liquor1 = Liquor(name = "버팔로 트레이스", LiquorRating.VERY_GOOD, review = "review", category = "cate")
+        val liquor2 = Liquor(name = "메이커스 마크", LiquorRating.VERY_GOOD, review = "review", category = "cate")
+        val liquor3 = Liquor(name = "와일드 터키", LiquorRating.VERY_GOOD, review = "review", category = "cate")
+        liquorRepository.save(liquor1)
+        liquorRepository.save(liquor2)
+        liquorRepository.save(liquor3)
+
+        val findLiquors = liquorService.findLiquors()
+
+        assertThat(findLiquors).isEqualTo(listOf(LiquorResponse.toDto(liquor1), LiquorResponse.toDto(liquor2), LiquorResponse.toDto(liquor3)))
+    }
+
+
 }
