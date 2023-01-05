25e7f66ff2801957365030dca0a4cde31fe5b6ca
rnfkaus8 <kth4350@gmail.com>
2023-01-05 19:59:17 +0900

    add controller test : findOne

--- src/test/kotlin/record/liquors/liquor/api/LiquorApiControllerTest.kt
+++ src/test/kotlin/record/liquors/liquor/api/LiquorApiControllerTest.kt
@@ -14,11 +14,17 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import record.liquors.liquor.entity.Liquor
 import record.liquors.liquor.entity.LiquorRating
+import record.liquors.liquor.repository.LiquorRepository
+import record.liquors.liquor.service.LiquorService
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class LiquorApiControllerTest(
+    @Autowired
+    val liquorRepository: LiquorRepository,
+
     @Autowired
     val mockMvc: MockMvc,
 
@@ -33,12 +39,25 @@ class LiquorApiControllerTest(
                 rating = LiquorRating.VERY_GOOD,
                 review = null,
                 category = null
-
             )
         )
         mockMvc.perform(
             post("/liquor")
                 .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk)
+
+    }
+
+    @Test
+    fun findOne() {
+        val liquor = Liquor(name = "버팔로 트레이스", LiquorRating.VERY_GOOD, review = "review", category = "cate")
+        liquorRepository.save(liquor)
+        liquorRepository.flush()
+
+        mockMvc.perform(
+            get("/liquor/{id}", liquor.id)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
         ).andExpect(status().isOk)
