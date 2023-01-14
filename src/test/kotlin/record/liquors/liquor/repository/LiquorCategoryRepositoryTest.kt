8ab369630651b5841c3584bf757ded3f5021e0c0
rnfkaus8 <kth4350@gmail.com>
2023-01-14 20:52:09 +0900

    add to child category test

--- src/test/kotlin/record/liquors/liquor/repository/LiquorCategoryRepositoryTest.kt
+++ src/test/kotlin/record/liquors/liquor/repository/LiquorCategoryRepositoryTest.kt
@@ -1,13 +1,14 @@
 package record.liquors.liquor.repository
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
 import record.liquors.liquor.entity.LiquorCategory
 
 @SpringBootTest
+@Transactional
 class LiquorCategoryRepositoryTest(
     @Autowired val liquorCategoryRepository: LiquorCategoryRepository
 ) {
@@ -23,6 +24,27 @@ class LiquorCategoryRepositoryTest(
 
         // then
         assertThat(findCategory).isEqualTo(category)
+    }
+
+    @Test
+    fun `category child save`() {
+        //given
+        val parent = LiquorCategory("위스키")
+        val child = LiquorCategory("버번 위스키", parent = parent)
+        parent.addChildCategory(child)
+
+        liquorCategoryRepository.save(parent)
+        liquorCategoryRepository.save(child)
+
+        //when
+        val findParent = liquorCategoryRepository.findById(parent.id!!).get()
+        val findChild = liquorCategoryRepository.findById(child.id!!).get()
+
+        //then
+        assertThat(findParent).isEqualTo(parent)
+        assertThat(findParent.children).containsAll(listOf(findChild))
+        assertThat(findChild.parent).isEqualTo(parent)
+
     }
 
 }
