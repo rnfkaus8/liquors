3183600b79ae50f8a2cd9bfe38027d64d992ce9f
rnfkaus8 <kth4350@gmail.com>
2023-01-21 16:34:15 +0900

    리뷰 추가에 리뷰 응답 코드 변경

--- src/main/kotlin/record/liquors/liquor/api/LiquorResponse.kt
+++ src/main/kotlin/record/liquors/liquor/api/LiquorResponse.kt
@@ -3,13 +3,14 @@ package record.liquors.liquor.api
 import record.liquors.liquor.entity.Liquor
 import record.liquors.liquor.entity.LiquorCategory
 import record.liquors.liquor.entity.LiquorRating
+import record.liquors.liquor.entity.Review
 
 data class LiquorResponse(
     val name: String,
 
     val rating: LiquorRating,
 
-    var review: String?,
+    var review: MutableList<Review>,
 
     var category: LiquorCategory,
 
