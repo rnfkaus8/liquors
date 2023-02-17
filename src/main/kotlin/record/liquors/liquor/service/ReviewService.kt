6d8dbc54f07a13278b59617bb96e4a143a843e37
rnfkaus8 <kth4350@gmail.com>
2023-02-17 14:21:35 +0900

    reviewService 추가 - 엔티티 저장

---
+++ src/main/kotlin/record/liquors/liquor/service/ReviewService.kt
@@ -0,0 +1,18 @@
+package record.liquors.liquor.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import record.liquors.liquor.entity.Review
+import record.liquors.liquor.repository.ReviewRepository
+
+@Service
+@Transactional(readOnly = true)
+class ReviewService(
+    val reviewRepository: ReviewRepository
+) {
+
+    fun save(request: Review): Long {
+        reviewRepository.save(request)
+        return request.id!!
+    }
+}
