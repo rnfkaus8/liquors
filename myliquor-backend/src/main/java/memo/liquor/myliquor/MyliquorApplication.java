b1be6bb2c82fc0f7782a0ac0f173819420917080
rnfkaus8 <kth4350@gmail.com>
2023-02-27 23:15:54 +0900

    start liquor 진짜 최종

---
+++ myliquor-backend/src/main/java/memo/liquor/myliquor/MyliquorApplication.java
@@ -0,0 +1,13 @@
+package memo.liquor.myliquor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyliquorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyliquorApplication.class, args);
+	}
+
+}
