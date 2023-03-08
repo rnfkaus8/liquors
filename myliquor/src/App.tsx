22d108079d3b51a5754c62cb342b7bc69cf907c2
rnfkaus8 <kth4350@gmail.com>
2023-03-08 23:26:28 +0900

    네비게이션 네임을 RouteName 으로 변경

--- myliquor/src/App.tsx
+++ myliquor/src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Liquor from './screens/Liquor';
+import SaveLiquor from './screens/SaveLiquor';
+import {RouteName} from './asset/navigation';
 
 const Stack = createNativeStackNavigator();
 const App: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Liquor" component={Liquor} />
+        <Stack.Screen name={RouteName.Liquor} component={Liquor} />
+        <Stack.Screen name={RouteName.SaveLiquor} component={SaveLiquor} />
       </Stack.Navigator>
     </NavigationContainer>
   );
