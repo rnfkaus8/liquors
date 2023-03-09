035ce9750a1f9857279301d35708c26011567232
rnfkaus8 <kth4350@gmail.com>
2023-03-09 21:18:15 +0900

    초기 화면 및 주류 추가 페이지 POC 1차

---
+++ myliquor/src/screens/SaveLiquor.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const SaveLiquor: React.FC = () => {
+  return (
+    <View>
+      <Text>내가 마신 주류를 저장하는 페이지입니당</Text>
+    </View>
+  );
+};
+
+export default SaveLiquor;
