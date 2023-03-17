15ca54487100720530c8ab28278fdde974307c72
rnfkaus8 <kth4350@gmail.com>
2023-03-17 19:51:32 +0900

    liquor entity 응답 객체 추가

---
+++ myliquor-backend/src/main/java/memo/liquor/myliquor/controller/LiquorResponse.java
@@ -0,0 +1,16 @@
+package memo.liquor.myliquor.controller;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import memo.liquor.myliquor.entity.Category;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class LiquorResponse {
+  private Long id;
+  private final String name;
+  private final Category category;
+
+}
