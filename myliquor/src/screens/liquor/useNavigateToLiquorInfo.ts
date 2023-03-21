146919387f02aa0d6140aa9dfcff215b2848b2a5
rnfkaus8 <kth4350@gmail.com>
2023-03-21 20:44:33 +0900

    주류 상세정보 페이지로 이동하는 훅

---
+++ myliquor/src/screens/liquor/useNavigateToLiquorInfo.ts
@@ -0,0 +1,13 @@
+import {useCallback} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {AppNavigationProp, RouteName} from '../../asset/navigation';
+
+export interface LiquorProps {
+  liquorId: number;
+}
+export const useNavigateToLiquorInfo = () => {
+  const navigation = useNavigation<AppNavigationProp>();
+  return useCallback(({liquorId}: LiquorProps) => {
+    navigation.navigate(RouteName.LiquorInfo, {liquorId});
+  }, []);
+};
