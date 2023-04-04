bf0212bfb71993c713f84cb9ec4bf40b7eda3d1c
rnfkaus8 <kth4350@gmail.com>
2023-04-04 20:44:43 +0900

    fetch join 추가

--- myliquor-backend/src/main/java/memo/liquor/myliquor/repository/LiquorRepository.java
+++ myliquor-backend/src/main/java/memo/liquor/myliquor/repository/LiquorRepository.java
@@ -2,6 +2,11 @@ package memo.liquor.myliquor.repository;
 
 import memo.liquor.myliquor.entity.Liquor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LiquorRepository extends JpaRepository<Liquor, Long> {
+  @Query("select l from Liquor l join fetch l.category")
+  List<Liquor> findAllLiquor();
 }
