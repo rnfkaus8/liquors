f7c2d56ec5e6f063e4ffe802c9e134095c0c1286
rnfkaus8 <kth4350@gmail.com>
2023-04-11 21:15:28 +0900

    파일 저장 properties 객체 추가

---
+++ myliquor-backend/src/main/java/memo/liquor/myliquor/StorageProperties.java
@@ -0,0 +1,15 @@
+package memo.liquor.myliquor;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+@Getter
+@Setter
+public class StorageProperties {
+
+  private String location = "/Users/kimtaehyun/study/liquors/uploadImage";
+
+}
