280f5cb07f7fae64a58d735f015c4b71517a2d12
rnfkaus8 <kth4350@gmail.com>
2023-04-14 10:10:44 +0900

    add EnableConfigurationProperties

--- myliquor-backend/src/main/java/memo/liquor/myliquor/MyliquorApplication.java
+++ myliquor-backend/src/main/java/memo/liquor/myliquor/MyliquorApplication.java
@@ -2,12 +2,14 @@ package memo.liquor.myliquor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class MyliquorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyliquorApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MyliquorApplication.class, args);
+  }
 
 }
